903b3593d82316e6b8e4f6cc9acb8915aa245417
Unknown <luke.renegar@gmail.com>
2018-02-25 17:42:12 -0500

    Added config files
    Added configuration files for server deployment.

---
+++ deploy/gunicorn_cfg.py
@@ -0,0 +1,5 @@
+import multiprocessing
+
+bind = "unix:/run/bits-server/bits.sock"
+pidfile = "/run/bits-server/bits.pid"
+workers = 2
---
+++ deploy/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /run/bits-server
+chown bits:bits /run/bits-server
