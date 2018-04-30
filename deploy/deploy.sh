f2cd1d7ba1c3727422e41217b95fd5e7d0c85700
Zach <zrb@umd.edu>
2018-04-30 19:58:47 -0400

    set additional config path

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp /srv/bits-server/deploy/nginx.conf /etc/nginx/sites-available/bits-server
+cp /srv/bits-server/deploy/nginx.conf /etc/nginx/conf.d/bits-server.conf
+
 cp /srv/bits-server/deploy/supervisord.conf /etc/supervisor/conf.d/bits-server.conf
