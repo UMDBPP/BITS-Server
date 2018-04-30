799cb193970786f9e2c5863a89e15994505b8448
Zach <zrb@umd.edu>
2018-04-30 19:15:18 -0400

    absolute paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp nginx.conf /etc/nginx/sites-available/bits-server
-cp supervisord.conf /etc/supervisor/conf.d/bits-server.conf
+cp /srv/bits-server/deploy/nginx.conf /etc/nginx/sites-available/bits-server
+cp /srv/bits-server/deploy/supervisord.conf /etc/supervisor/conf.d/bits-server.conf
